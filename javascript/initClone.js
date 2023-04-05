634350387ae6aa3a438082a033282058524baf4c
Charles Sin <charlessin1993@gmail.com>
2023-04-05 13:07:31 +0800

    1. update init clone logic.

--- javascript/initClone.js
+++ javascript/initClone.js
@@ -4,15 +4,22 @@ export function initCloneMission() {
   const USER = JSON.parse(window.localStorage.getItem("googleUser")) || null;
   const LOCALDATA = JSON.parse(window.localStorage.getItem("kanban-data")) || null;
 
-  if (!LOCALDATA) {
-    if (!USER) {
-      readFirestoreData("kanban-data").then((dummy) => {
+  if (LOCALDATA) {
+    if (USER) {
+      localStorage.removeItem("kanban-data");
+      readFirestoreData("kanban-data", `${USER.uid}`).then((dummy) => {
+        const { data } = dummy;
+        localStorage.setItem("kanban-data", data);
+      });
+    }
+  } else {
+    if (USER) {
+      readFirestoreData("kanban-data", `${USER.uid}`).then((dummy) => {
         const { data } = dummy;
         localStorage.setItem("kanban-data", data);
       });
     } else {
-      localStorage.removeItem("kanban-data");
-      readFirestoreData("kanban-data", `${USER.uid}`).then((dummy) => {
+      readFirestoreData("kanban-data").then((dummy) => {
         const { data } = dummy;
         localStorage.setItem("kanban-data", data);
       });
